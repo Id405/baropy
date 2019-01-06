bea9c0ccef0faa976eda2fb45be36eca25944ef4
ninja <8o7wermobile@gmail.com>
2019-01-06 10:16:11 -0900

    Added the ability to get players by name, id, and ip, and also added methods to the player class such as banning, and giving ranks or permissions. Also changed the version number to 0.1.1

--- baropy/__init__.py
+++ baropy/__init__.py
@@ -5,12 +5,12 @@ import os
 from threading import Thread
 
 class Barotrauma:
-    def __init__(self, stdinpath, udspath, writetimeout=0.1, responsetime=0.1):
+    def __init__(self, stdinpath, udspath, writetimeout=0.1, responsetime=0.1, buffersize=128):
         self.stdinpath = stdinpath
         self.udspath = udspath
         self.writetimeout = writetimeout
         self.responsetime = 0.3
-        self.udsbuffer = Udsbuffer()
+        self.udsbuffer = Udsbuffer(size = buffersize)
         self.__start_uds_thread()
 
     def __uds_thread(self):
@@ -20,9 +20,8 @@ class Barotrauma:
         except Exception as error:
             raise Exception("Error connecting to unix domain socket: " + repr(error))
         while True:
-            data = []
             data = ""
-            while True:
+            while True: # Recieve exactly one line of data
                 data = data + sock.recv(1).decode()
                 if data.endswith("\n"):
                     break
@@ -36,10 +35,9 @@ class Barotrauma:
         try:
             with open(self.stdinpath, "w") as file:
                 file.write(command + " ".join(args))
-                time.sleep(self.writetimeout)
                 for string in followup:
-                    file.write(string)
                     time.sleep(self.writetimeout)
+                    file.write(string)
         except Exception as error:
             raise Exception("Error on writing to pipefile: " + repr(error))
 
@@ -51,9 +49,9 @@ class Barotrauma:
 
         response = self.udsbuffer.buffer
 
-        index = [i for i, s in enumerate(response) if command in s][0]
+        index = [i for i, s in enumerate(response) if command in s][0] #Get index of command in game output
 
-        response = response[index+1:]
+        response = response[index+1:] #trim buffer array so that its only text after the command in game output
 
         return response
 
@@ -63,7 +61,7 @@ class Barotrauma:
     def ban_ip(self, ip, reason, duration):
         self.send_command("banip", [ip], [reason, duration])
 
-    def get_clients(self):
+    def get_players(self):
         responses = self.response("clientlist")
         responses = [i for i in responses if i.startswith("-")]
         clients = []
@@ -75,16 +73,39 @@ class Barotrauma:
                 name = response[response.find(":")+2:response.find("playing")-1]
             id = response[:response.find(":")]
             ip = response[response.rfind(",")+2:].strip()
-            clients.append(Player(name, id, ip))
+            clients.append(Player(self, name, id, ip))
 
         return clients
 
-class Udsbuffer:
+    def get_player_by_name(self, name): #returns -1 if a client cant be found
+        clients = self.get_players()
+        for client in clients:
+            if client.name.lower() == name.lower():
+                return client
+        return -1
+
+    def get_player_by_ip(self, ip): #same as above
+        clients = self.get_players()
+        for client in clients:
+            if client.ip == ip:
+                return ip
+        return -1
+
+    def get_player_by_id(self, id): #Id must be a string
+        clients = self.get_players()
+        for client in clients:
+            if client.id == id:
+                return client
+        return -1
+
+
+
+class Udsbuffer: #Holds the buffer for incoming lines of data from the game server
     def __init__(self, size=128):
         self.buffer = []
-        self.size = 128
+        self.size = size
 
-    def add(self, data):
+    def add(self, data): #Please use this function insteada of udsbuffer.append.(Data) as this function limits the size of the list
         self.buffer.append(data)
         if len(self.buffer) > self.size:
             del self.buffer[self.size:]
@@ -93,7 +114,20 @@ class Udsbuffer:
         self.buffer.clear()
 
 class Player:
-    def __init__(self, name, id, ip):
+    def __init__(self, barotrauma, name, id, ip):
         self.name = name
         self.id = id
         self.ip = ip
+        self.barotrauma = barotrauma
+
+    def ban_name(self, reason, duration):
+        barotrauma.ban_name(self.name, reason, duration)
+
+    def ban_ip(self, reason, duration):
+        barotrauma.ban_name(self.ip, reason, duration)
+
+    def give_rank(self, rank):
+        barotrauma.send_command(give_rank, [id], [rank])
+
+    def give_permission(self, permission):
+        barotrauma.send_command(give_rank, [id], [permission])
--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from distutils.core import setup
 
 setup(
     name='baropy',
-    version='0.1dev',
-    description='Interface python and barotrauma'
-    author='8o7wer'
-    url='https://github.com/8o7wer/baropy'
+    version='0.1.1dev',
+    description='Interface python and barotrauma',
+    author='8o7wer',
+    url='https://github.com/8o7wer/baropy',
     packages=['baropy'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license'
 )
